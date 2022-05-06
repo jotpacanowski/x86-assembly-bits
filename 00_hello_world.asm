6e34c42cba3702fe53d22c01f448d1f8982f1d8a
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2022-05-06 12:28:13 +0200

    Clean up hello world

--- 00_hello_world.asm
+++ 00_hello_world.asm
@@ -4,7 +4,9 @@ extern  printf
 extern  scanf
 
 section .data
-	fmt_hello	db 'Hello world!', 0xA, 0
+	prompt_hello	db 'Hello world!', 0xA, 0
+	prompt_int	db 'Enter an integer: ', 0
+	prompt_float	db 'Enter a floating-point number: ', 0
 	fmt_int		db '%d', 0
 	fmt_int_lf	db '%d', 10, 0
 	fmt_float	db '%lf', 0
@@ -16,51 +18,44 @@ align 16
 	dblarr resq 4
 
 section .text
-global  main
+global main
 main:
 	sub	rsp, 16
 
-	call main_hello
-	call main_int_echo
-	call main_float_echo
+	call	main_hello
+	call	main_int_echo
+	call	main_float_echo
 
-	add     rsp, 16
-	xor     rax, rax
+	add	rsp, 16
+	xor	rax, rax
 	ret
 
 main_hello:
-	lea     rdi, [fmt_hello]
-	mov     al, 0
-	call    printf wrt ..plt
+	lea	rdi, [prompt_hello]
+	mov	al, 0
+	call	printf wrt ..plt
 	ret
 
 main_int_echo:
-	push rax
-	mov	[rsp], dword 0x00203e69      ; 'i> ' 3e,3f
-	lea	rdi, [rsp]
+	lea	rdi, [prompt_int]
 	mov	al, 0
-	call printf wrt ..plt
-	pop	rax
-	lea     rsi, [intarr+0]
+	call	printf wrt ..plt
+
+	lea	rsi, [intarr+0]
 	lea	rdi, [fmt_int]
 	mov	al, 0
 	call	scanf wrt ..plt
-	push	rax
-	mov	[rsp], dword 0x000a6425   ; "%d\n"
-	mov	rsi, [intarr+0]
-	mov	rdi, rsp
+
+	lea	rdi, [fmt_int_lf]
 	mov	al, 0
 	call	printf wrt ..plt
-	pop	rax
+
 	ret
 
 main_float_echo:
-	push	rax
-	mov	[rsp], dword 0x00203e66
-	lea	rdi, [rsp]
+	lea	rdi, [prompt_float]
 	mov	al, 0
 	call	printf wrt ..plt
-	pop	rax
 
 	lea	rsi, [dblarr+0]
 	lea	rdi, [fmt_float]
